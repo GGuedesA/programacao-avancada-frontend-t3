f53c43cc0d2c44dd781e45165a7252a74cb749c9
Gabriel Guedes <gabrielguedes708@gmail.com>
2023-07-25 21:02:06 -0500

    att. 5.0

--- bootstrap-project/scripts.js
+++ bootstrap-project/scripts.js
@@ -119,9 +119,42 @@ containerC.animate(1.0);
 containerD.animate(1.0); 
 
 
-
-
-
 // Parallax
 $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
 $('#apply-content').parallax({imageSrc: 'img/pattern.png'})
+
+
+// Scroll para as seções da página a partir do menu
+
+let navBtn = $('.nav-item');
+
+let bannerSection = $('#carousel-area');
+let aboutSection = $('#about-area');
+let servicesSection = $('#services-area');
+let teamSection = $('#team-area');
+let portfolioSection = $('#portfolio-area');
+let contactSection = $('#contact-area');
+
+let scrollTo = '';
+
+$(navBtn).click(function() {
+  
+  let btnId = $(this).attr('id');
+  
+  if(btnId == 'about-menu') {
+    scrollTo = aboutSection;
+  } else if(btnId == 'services-menu') {
+    scrollTo = servicesSection;
+  } else if(btnId == 'team-menu') {
+    scrollTo = teamSection;
+  } else if(btnId == 'portfolio-menu') {
+    scrollTo = portfolioSection;
+  } else if(btnId == 'contact-menu') {
+    scrollTo = contactSection;
+  } else {
+    scrollTo = bannerSection;
+  }
+  
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $(scrollTo).offset().top - 70
+  }, 1500);});
